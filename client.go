5600c25ea0cf66ff1e61de4399139d976df5d28b
xizan.li <xizan@office.feng.com>
2018-11-30 14:47:22 +0800

    支持文件上传

--- client.go
+++ client.go
@@ -1,42 +1,50 @@
 package hasaki
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/json-iterator/go"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"reflect"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
+type ContentType string
+
+const (
+	FORM_TYPE ContentType = "application/x-www-form-urlencoded"
+	JSON_TYPE             = "application/json; charset=utf-8"
+)
+
 type Client struct {
+	requestParam *RequestParam
+	requestBody  io.Reader
 	link         string
-	contentType  string
+	contentType  ContentType
 	method       string
-	timeout      time.Duration
-	retryTimes   int
 	headers      Form
-	form         JSON
-	responseBody []byte
-	statusCode   int
+	response     *http.Response
 	httpClient   *http.Client
 }
 
-type RequestOption struct {
+type RequestParam struct {
+	ContentType
 	TimeOut    time.Duration
 	RetryTimes int
 	ProxyURL   string
 }
 
-func Request(method string, URL string, opt *RequestOption) *Client {
+func Request(method string, URL string, opt *RequestParam) *Client {
 	if opt == nil {
-		opt = &RequestOption{
-			TimeOut:    2 * time.Second,
-			RetryTimes: 3,
+		opt = &RequestParam{
+			TimeOut:     2 * time.Second,
+			RetryTimes:  3,
+			ContentType: FORM_TYPE,
 		}
 	} else {
 		if opt.TimeOut == 0 {
@@ -45,16 +53,18 @@ func Request(method string, URL string, opt *RequestOption) *Client {
 		if opt.RetryTimes == 0 {
 			opt.RetryTimes = 3
 		}
+		if opt.ContentType == "" {
+			opt.ContentType = FORM_TYPE
+		}
 	}
 
 	var client = &Client{
-		headers:    Form{},
-		form:       JSON{},
-		method:     strings.ToUpper(method),
-		link:       URL,
-		timeout:    opt.TimeOut,
-		retryTimes: opt.RetryTimes,
-		httpClient: http.DefaultClient,
+		requestBody:  strings.NewReader(""),
+		headers:      Form{},
+		requestParam: opt,
+		method:       strings.ToUpper(method),
+		link:         URL,
+		httpClient:   http.DefaultClient,
 	}
 	client.httpClient.Timeout = opt.TimeOut
 
@@ -71,34 +81,20 @@ func Request(method string, URL string, opt *RequestOption) *Client {
 	return client
 }
 
-func Get(url string, opt *RequestOption) *Client {
+func Get(url string, opt *RequestParam) *Client {
 	return Request("GET", url, opt)
 }
 
-func Put(url string, opt *RequestOption) *Client {
+func Put(url string, opt *RequestParam) *Client {
 	return Request("PUT", url, opt)
 }
 
-func Post(url string, opt *RequestOption) *Client {
+func Post(url string, opt *RequestParam) *Client {
 	return Request("POST", url, opt)
 }
 
-func Delete(url string, opt *RequestOption) *Client {
+func Delete(url string, opt *RequestParam) *Client {
 	return Request("DELETE", url, opt)
-}
-
-func (this *Client) Type(contentType string) *Client {
-	switch contentType {
-	case "json":
-		this.contentType = "json"
-		this.headers["Content-Type"] = "application/json; charset=utf-8"
-		break
-	default:
-		this.contentType = "json"
-		this.headers["Content-Type"] = "application/x-www-form-urlencoded"
-		break
-	}
-	return this
 }
 
 func (this *Client) Set(headers Form) *Client {
@@ -108,13 +104,61 @@ func (this *Client) Set(headers Form) *Client {
 	return this
 }
 
+func (this *Client) SetBody(r io.Reader) *Client {
+	this.requestBody = r
+	return this
+}
+
 func (this *Client) Send(data JSON) *Client {
-	this.form = data
+	if this.requestParam.ContentType == JSON_TYPE {
+		body, _ := jsoniter.Marshal(data)
+		this.requestBody = bytes.NewReader(body)
+		return this
+	}
+
+	var form = url.Values{}
+	for k, v := range data {
+		var item = make([]string, 0)
+		var varType = reflect.TypeOf(v).String()
+
+		switch varType {
+		case "[]string":
+			item = v.([]string)
+			form[k+"[]"] = item
+			break
+		case "[]int":
+			arr := v.([]int)
+			for _, num := range arr {
+				item = append(item, strconv.Itoa(num))
+			}
+			form[k+"[]"] = item
+			break
+		case "int":
+			tmp, _ := v.(int64)
+			form.Set(k, strconv.Itoa(int(tmp)))
+			break
+		default:
+			tmp, _ := v.(string)
+			form.Set(k, tmp)
+			break
+		}
+	}
+
+	qs := form.Encode()
+	if this.method == "GET" || this.method == "DELETE" {
+		u, _ := url.Parse(this.link)
+		if u.RawQuery != "" {
+			qs += "&" + u.RawQuery
+		}
+		this.link = u.Scheme + "://" + u.Host + u.Path + "?" + qs
+	} else {
+		this.requestBody = strings.NewReader(qs)
+	}
 	return this
 }
 
 func (this *Client) GetResponse() (resp *http.Response, err error) {
-	for i := 0; i < this.retryTimes; i++ {
+	for i := 0; i < this.requestParam.RetryTimes; i++ {
 		resp, err = this.query()
 		if err == nil {
 			break
@@ -124,75 +168,32 @@ func (this *Client) GetResponse() (resp *http.Response, err error) {
 }
 
 func (this *Client) query() (*http.Response, error) {
-	var form = url.Values{}
-	var err error
-	for k, v := range this.form {
-		var item = make([]string, 0)
-		var varType = reflect.TypeOf(v).String()
-		if varType == "[]string" {
-			item = v.([]string)
-		} else if varType == "[]int" {
-			arr := v.([]int)
-			for _, num := range arr {
-				item = append(item, strconv.Itoa(num))
-			}
-		} else if varType == "int" {
-			item = append(item, strconv.Itoa(int(reflect.ValueOf(v).Int())))
-		} else {
-			item = append(item, reflect.ValueOf(v).String())
-		}
-
-		if varType == "[]string" || varType == "[]int" {
-			form[k+"[]"] = item
-		} else {
-			form[k] = item
-		}
-	}
-
-	var req = &http.Request{}
-	queryString := form.Encode()
-	if (this.method == "GET" || this.method == "DELETE") && queryString != "" {
-		re, _ := regexp.Compile(`\?.*?=.*?`)
-		if re.MatchString(this.link) {
-			this.link = this.link + "&" + queryString
-		} else {
-			this.link = this.link + "?" + queryString
-		}
-		req, err = http.NewRequest(this.method, this.link, nil)
-	} else {
-		if this.contentType == "json" {
-			bytes, _ := jsoniter.Marshal(this.form)
-			req, err = http.NewRequest(this.method, this.link, strings.NewReader(string(bytes)))
-		} else {
-			req, err = http.NewRequest(this.method, this.link, strings.NewReader(queryString))
-		}
-		if this.contentType == "" {
-			this.headers["Content-Type"] = "application/x-www-form-urlencoded"
-		}
+	var req, err = http.NewRequest(this.method, this.link, this.requestBody)
+	if err != nil {
+		return nil, err
 	}
 
 	for k, v := range this.headers {
 		req.Header.Set(k, v)
 	}
-
-	if err != nil {
-		return nil, err
+	if this.requestParam.ContentType != "" {
+		req.Header.Set("Content-Type", string(this.requestParam.ContentType))
 	}
 
 	res, err := this.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	body, _ := ioutil.ReadAll(res.Body)
-	this.responseBody = body
-	this.statusCode = res.StatusCode
-	defer res.Body.Close()
+	this.response = res
 	return res, err
 }
 
 func (this *Client) GetBody() ([]byte, error) {
-	_, err := this.GetResponse()
-	return this.responseBody, err
+	res, err := this.query()
+	if err != nil {
+		return []byte{}, err
+	}
+	return ioutil.ReadAll(res.Body)
 }
 
 func (this *Client) Json() (JSON, error) {
@@ -211,6 +212,9 @@ func (this *Client) Json() (JSON, error) {
 }
 
 func (this *Client) GetStatusCode() (int, error) {
-	_, err := this.GetResponse()
-	return this.statusCode, err
+	res, err := this.query()
+	if err != nil {
+		return 0, err
+	}
+	return res.StatusCode, nil
 }
